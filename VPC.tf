26c4bc9c45b0e9a41940f993869972a3cf16a77a
ravindrakoyi <ravindrababu.koyi@gmail.com>
2021-11-03 17:19:22 +0000

    Updated VPC module

--- VPC.tf
+++ VPC.tf
@@ -1,8 +1,8 @@
-network_name="ravi-network"
-project-id="var.project"
+network_name="gaurav-network"
+project_id="var.project"
 subnets = [
   {
-    subnet_name   = "ravi-subnet"
+    subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
